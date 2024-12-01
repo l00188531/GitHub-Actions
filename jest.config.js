fedcc7b0123646150d3e951390715b695ab1cf52
l00188531 <l00188531@atu.ie>
2024-12-01 16:42:55 +0000

    jest config added to fix the file path error

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testMatch: ['<rootDir>/tests/**/*.js'], // Match all .js files in the 'tests' folder
+    // Alternative: Use a custom regex if needed
+    // testRegex: 'tests/.*\\.js$',
+  };
+  
+
+
